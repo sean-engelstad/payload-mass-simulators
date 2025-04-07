638d45c38999343b7d02c2a023b2d29bdc370522
Micah Turner <micahturner133@Micahs-MacBook-Pro.local>
2025-04-07 15:20:39 -0400

    Continued progress on verifying modal analysis

--- tests/cantilever/tacs-analysis.py
+++ tests/cantilever/tacs-analysis.py
@@ -35,17 +35,38 @@ E = 70.0e9  # Young's modulus (Pa)
 nu = 0.3  # Poisson's ratio
 ys = 270.0e6  # yield stress
 
+sigma = 1e-2 # eigenvalue guess
+
 # Shell thickness
 # A = 0.1  # m
 # Iz = 0.2  # m
 # Iy = 0.3  # m
 # J = 0.4
-b = h = 5e-3
-A = b * h
-Iy = Iz = b * h**3 / 12.0
-J = 2 * Iz
+b = h = 1e-3
 
-kTransverse = 1e3 # orig 1000, but want to make it high so becomes Euler-Bernoulli
+
+# Trial 1
+# A = b * h
+# A = A * 10000
+# Iy = 0
+# Iz = b * h**3 / 12.0
+# J = 1e4 * Iz
+
+# Trial 2
+# A = b * h
+# Iy = 0
+# Iz = b * h**3 / 12.0
+# J = 2 * Iz 
+
+# Trial 3
+A = b * h
+Iy = 0
+Iz = b * h**3 / 12.0
+J = Iy + Iz
+
+
+
+kTransverse = 1e5 # orig 1000, but want to make it high so becomes Euler-Bernoulli
 
 # Callback function used to setup TACS element objects and DVs
 def elemCallBack(dvNum, compID, compDescript, elemDescripts, globalDVs, **kwargs):
@@ -81,7 +102,7 @@ FEAAssembler.initialize(elemCallBack)
 # Static problem
 evalFuncs = ["mass", "ks_vmfailure"]
 
-MP = FEAAssembler.createModalProblem("modal", sigma=10.0, numEigs=10)
+MP = FEAAssembler.createModalProblem("modal", sigma=sigma, numEigs=10)
 MP.setOption("printLevel", 2)
 MP.solve()
 
