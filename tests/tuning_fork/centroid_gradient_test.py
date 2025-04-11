7772cc2e463fd84bb14566bac6fe4564343ab464
Toby Roule <tjroule@gmail.com>
2025-04-11 15:15:15 -0400

    added thickness derivatives to CG gradient calcs

--- tests/tuning_fork/centroid_gradient_test.py
+++ tests/tuning_fork/centroid_gradient_test.py
@@ -1,16 +1,22 @@
 import sys, numpy as np, os, time
 from payload_mass_sim import *
 
-x = np.array([2,1,1] * 3)
+x = np.array([2,1,1] * 9)
 # x = np.array([2,1,1])
 
+# tree = TreeData(
+#     tree_start_nodes=[0] + [1],
+#     tree_directions=[5] + [0],
+#     # tree_start_nodes=[0],
+#     # tree_directions=[5],
+#     nelem_per_comp=10,
+# )
+
 tree = TreeData(
-    tree_start_nodes=[0] + [1] + [0],
-    tree_directions=[5] + [0] + [1],
-    # tree_start_nodes=[0],
-    # tree_directions=[5],
-    nelem_per_comp=10,
-)
+        tree_start_nodes=[0] + [1]*4 + [2,3,4,5],
+        tree_directions=[5] + [0,1,2,3] + [5]*4,
+        nelem_per_comp=10
+    )
 
 centroid = tree.get_centroid(x)
 print(f"{centroid=}")
